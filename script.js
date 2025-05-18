3981c34e2c82ccf3c551874faf44552923d84490
SHERLIN2082 <sherlinjenifer8@gmail.com>
2025-05-18 22:35:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-class SegmentDigit {
+class SegmentDigit{
     constructor(container) {
       this.container = container;
       this.segments = {};
-  
       const segmentList = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
       segmentList.forEach(s => {
         const el = document.createElement('div');
@@ -10,9 +9,8 @@ class SegmentDigit {
         container.appendChild(el);
         this.segments[s] = el;
       });
-    }
-  
-    setDigit(num) {
+}
+    setDigit(num){
       const segmentMap = {
         0: 'abcdef',
         1: 'bc',
@@ -24,20 +22,17 @@ class SegmentDigit {
         7: 'abc',
         8: 'abcdefg',
         9: 'abcfgd'
-      };
-  
+    };
       const active = segmentMap[num] || '';
-      for (let s in this.segments) {
+      for (let s in this.segments){
         this.segments[s].classList.toggle('on', active.includes(s));
       }
     }
-  }
-  
-  class Clock {
+}
+class Clock{
     constructor(container) {
       this.container = container;
       this.digits = [];
-  
       for (let i = 0; i < 6; i++) {
         const digit = document.createElement('div');
         digit.classList.add('digit');
@@ -45,37 +40,31 @@ class SegmentDigit {
         this.digits.push(digitObj);
         container.appendChild(digit);
   
-        if (i === 1 || i === 3) {
+        if (i === 1 || i === 3){
           const colon = document.createElement('div');
           colon.className = 'colon';
           colon.innerText = ':';
           container.appendChild(colon);
         }
       }
-  
       this.update();
       setInterval(() => this.update(), 1000);
     }
-  
-    update() {
+    update(){
       const now = new Date();
       const timeStr = now.toTimeString().slice(0, 8).replace(/:/g, '');
       [...timeStr].forEach((digit, i) => {
         this.digits[i].setDigit(parseInt(digit));
       });
     }
-  }
-  
-  function startClock() {
+}
+function startClock(){
     const name = document.getElementById("nameInput").value.trim();
     if (!name) return alert("Please enter your name!");
-  
     document.getElementById("greeting").innerText = `Hello,${name}! `;
-    document.querySelector(".input-container").style.display = 'none';
-  
-    
+    document.querySelector(".input-container").style.display = 'none';  
   new Clock(document.getElementById("clock"));
-  }
+}
   
   
   
